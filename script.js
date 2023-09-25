55a043f3577cda8efcf1719ca9d3a00e24200555
Celine Hsieh <69034494+celine-hsieh@users.noreply.github.com>
2023-09-25 13:35:47 +0800

    092523

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-
-//window.addEventListener('load', yourFunction, false);
-// OR: 
-//window.addEventListener('DOMContentLoaded', yourFunction, false);
-
-// if (typeof window !== "undefined") {
-//   document
-//     .getElementById("cameraFileInput")
-//     .addEventListener("change", function () {
-//       document
-//         .getElementById("pictureFromCamera")
-//         .setAttribute("src", window.URL.createObjectURL(this.files[0]));
-//     });
-// }
-// else {
-//   console.log("In nodeJS");
-// }
-
 document
   .getElementById("cameraFileInput")
   .addEventListener("change", function () {
