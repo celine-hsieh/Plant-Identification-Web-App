f4c19366f50aa545ad6ad517962dd872198ee235
Celine Hsieh <69034494+celine-hsieh@users.noreply.github.com>
2023-09-27 15:58:37 +0800

    UI Design

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the current directory
 app.use('', express.static(__dirname));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
-// app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
+app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
 module.exports = app
